44842f873b54df4c7b4e7bca383cd55a576c924c
Arturo Rojas <quijadev@gmail.com>
2021-12-23 18:33:21 -0600

    Clase 61

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent{
 
-  constructor() { }
+  personajes:Personaje[] = [
+    {
+      nombre: 'Krillin',
+      poder: 9000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
 
-  ngOnInit(): void {
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0){ //validación para no enviar algo vacío
+      return;
+    }
+
+    console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);    
+    this.nuevo = {
+      nombre:'',
+      poder: 0
+    }
+
+    
+    //this.personajes.push();
+    //limpiar this formulario
   }
 
 }
